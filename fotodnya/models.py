f22b38a2ee88b6604152eec56858942ad3049ab9
juv4ik <juv4ik@gmail.com>
2021-04-30 00:20:22 +0300

    added model for foto added styles

--- fotodnya/models.py
+++ fotodnya/models.py
@@ -6,9 +6,8 @@ from django.db import models
 class fotoNews(models.Model):
     title = models.CharField(max_length=120)
     img = models.ImageField(upload_to='static/images/', null=True, blank=True, default='static/images/default.jpg')
-    author = models.CharField(max_length=120, blank=True, null=True)
-    url_from = models.URLField(max_length=300, default='http://fotodnya.site', blank=True, null=True)
-    #postdate = models.DateField(auto_now=False, auto_now_add=False)
+    author = models.CharField(max_length=120, blank=True)
+    url_from = models.URLField(max_length=300, default='http://fotodnya.site', blank=True)
     timepostdate = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     def __str__(self):
--- fotodnya/views.py
+++ fotodnya/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def indexPage(request):
+    posts = reversed(fotoNews.objects.all())
+    return render(request, 'index.html', {'posts': posts})
+
+
+
