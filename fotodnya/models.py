dec1264ebfb8ef564ff6bc7bf3458d8583382dc8
juv4ik <juv4ik@gmail.com>
2021-04-29 22:47:10 +0300

    added model for foto

--- fotodnya/models.py
+++ fotodnya/models.py
@@ -1,3 +1,17 @@
+import time
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+class fotoNews(models.Model):
+    title = models.CharField(max_length=120)
+    img = models.ImageField(upload_to='static/images/', null=True, blank=True, default='static/images/default.jpg')
+    author = models.CharField(max_length=120, blank=True, null=True)
+    url_from = models.URLField(max_length=300, default='http://fotodnya.site', blank=True, null=True)
+    #postdate = models.DateField(auto_now=False, auto_now_add=False)
+    timepostdate = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return f'{self.title}'
+
+
