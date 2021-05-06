adb3dd1c78ea36a6292472c1b2cbead6eb6f67b5
juv4ik <juv4ik@gmail.com>
2021-05-07 00:32:26 +0300

    added pagination

--- fotodnya/views.py
+++ fotodnya/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView
 from .models import *
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def indexPage(request):
-    posts = reversed(fotoNews.objects.all())
-    return render(request, 'index.html', {'posts': posts})
+    object_list = list(reversed(fotoNews.objects.all()))
+    paginator = Paginator(object_list, 8)  # 8 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'index.html',
+                  {'page': page,
+                   'posts': posts})
+
+# def indexPage(request):
+#    posts = reversed(fotoNews.objects.all())
+#    return render(request, 'index.html', {'posts': posts})
 
 class BlogDetailView(DetailView):
     model = fotoNews
@@ -13,6 +30,8 @@ class BlogDetailView(DetailView):
 
 def aboutPage(request):
     return render(request, 'about.html')
+
+
 
 
 
